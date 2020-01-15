f9b96bfe8074d3b28ee023daab676607f6ed4ee9
gamenic-ramesh <gamenic.ramesh@gmail.com>
2020-01-15 11:38:44 +0545

    aarthik patro begin

---
+++ aarthikpatro/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import AarthikPatro from './src/AarthikPatro';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AarthikPatro);
